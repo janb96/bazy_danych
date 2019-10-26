491b57c931ca3b506efc954e80c0c2b70eb4c33a
Sebastian Dąbek <sebekdabek@gmail.com>
2019-10-26 09:13:04 +0200

    Order status to SLA view
    add order statuses - In time or Late to the SLA view.

--- uvw_Order_Resolution_SLA.sql
+++ uvw_Order_Resolution_SLA.sql
@@ -18,8 +18,11 @@ select
 	,DATEDIFF(DAY, OrderDate, RequiredDate) as [Resolution Days to Required] 
 	,DATEDIFF(DAY, OrderDate, ShippedDate) as [Resolution Days to Shipped]
 	--,IIF(DATEDIFF(DAY, OrderDate, RequiredDate)-DATEDIFF(DAY, OrderDate, ShippedDate)>1) THEN 'cos' ELSE 'cos innego' END
+	,IIF(DATEDIFF(DAY, OrderDate, RequiredDate)>DATEDIFF(DAY, OrderDate, ShippedDate), 'In time', 'Late')
 	
 	from Orders o
+	--A lot of NULLs in Shipped Date in Orders
+	where ShippedDate is not null
 
 GO
 
