961bb686f19de7992623de90248ae53aa6c11c15
mariusz1345 <mariusz16@op.pl>
2019-11-27 23:14:51 +0100

    dodanie Customer-list

--- website/customer-list.php
+++ website/customer-list.php
@@ -1,38 +1,44 @@
-<?php
-include("connect.php");
-include("header.php");
-?>
+<?php include("connect.php") ?>
+<?php include("header.php"); ?>
+
 <table><a href='insert.php'><button type='submit' class='btn btn-primary'>INSERT DATA</button></a>
     </div></table></br>
 <table cellpadding='0' cellspacing='0' border='0' class='table table-striped table-bordered' >
     <tr><th>No</th>
-        <th width="125">Name</th>
-        <th>Title</th>
-        <th>Title Of Courtesy</th>
-        <th>Home Phone</th>
+        <th width="125">Company Name</th>
+        <th>Contact Name</th>
+        <th>Contact Title</th>
         <th>Address</th>
         <th>City</th>
+        <th>Region</th>
+        <th>PostalCode</th>
         <th>Country</th>
+        <th>Phone</th>
+        <th>Fax</th>
         <th colspan="2" >Option</th></tr>
     <?php
 
-    $sql_select = "SELECT EmployeeID, FirstName, LastName, Title,TitleOfCourtesy,HomePhone,Address,City,Country FROM Employees ORDER BY EmployeeID ASC";
+    $sql_select = "SELECT CompanyName, ContactName, ContactTitle, Address, City, Region, PostalCode, Country, Phone, Fax FROM Customers ORDER BY CustomerID ASC";
     $ret_select = $db->select($sql_select);
     $start_num = 0;
+
     foreach ($ret_select as $rw) {
         $start_num++;
         ?>
 
         <tr>
             <td><?php echo $start_num; ?></td>
-            <td><?php echo $rw['FirstName']; ?></td>
-            <td><?php echo $rw['LastName']; ?></td>
-            <td><?php echo $rw['Title']; ?></td>
-            <td><?php echo $rw['TitleOfCourtesy']; ?></td>
-            <td><?php echo $rw['HomePhone']; ?></td>
+            <td><?php echo $rw['CompanyName']; ?></td>
+            <td><?php echo $rw['ContactName']; ?></td>
+            <td><?php echo $rw['ContactTitle']; ?></td>
             <td><?php echo $rw['Address']; ?></td>
             <td><?php echo $rw['City']; ?></td>
+            <td><?php echo $rw['Region']; ?></td>
+            <td><?php echo $rw['PostalCode']; ?></td>
             <td><?php echo $rw['Country']; ?></td>
+            <td><?php echo $rw['Phone']; ?></td>
+            <td><?php echo $rw['Fax']; ?></td>
+
 
 
             <td> <a class='btn btn-info btn-sm' href="edit.php?edit=<?php echo $rw['EmployeeID']; ?>" title="edit">Edit</a>
@@ -43,3 +49,4 @@ include("header.php");
 </table>
 
 </div>
+
