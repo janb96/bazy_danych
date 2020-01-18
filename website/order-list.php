c3adc343f0c7a5520f0b2e6e58276cea6d9b84dc
janb96 <janekb1996@gmail.com>
2020-01-18 13:42:07 +0100

    [PC] Change link to add order

--- website/order-list.php
+++ website/order-list.php
@@ -2,7 +2,7 @@
 include("connect.php");
 include("header.php");
 ?>
-<table><a href='order-add.php'><button type='submit' class='btn btn-primary'>New order</button></a>
+<table><a href='order-add2.php'><button type='submit' class='btn btn-primary'>New order</button></a>
     </div></table></br>
 
 
