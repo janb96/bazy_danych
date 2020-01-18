64ddfd1a1482dac0be59d1437bd0e880e7a8ede8
janb96 <janekb1996@gmail.com>
2020-01-18 10:34:39 +0100

    [NF] New query

---
+++ website/stats5.php
@@ -0,0 +1,9 @@
+<?php
+
+include("connect.php");
+
+$sql_select = "SELECT EmployeeID, count(OrderID) as howMuch FROM `order_details` NATURAL JOIN `orders` NATURAL JOIN `employees` GROUP BY EmployeeID ORDER BY `howMuch`  DESC";
+
+$ret_select = $db->select($sql_select);
+
+var_dump($ret_select);
